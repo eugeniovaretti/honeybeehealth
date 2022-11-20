3734e494317fa9be9b30ee074b0899fb1232aefd
lucamainini <57671317+lucamainini@users.noreply.github.com>
2022-11-20 14:14:51 +0100

    main update

--- Map_visualization/server.R
+++ Map_visualization/server.R
@@ -53,8 +53,31 @@ X = X %>% tidyr::pivot_wider(
 states = X[1]
 X = X[2:9]
 
+#data <- inner_join(colony, stressor, by = c("year","months","state"))
+
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  
+  output$lost_table = DT::renderDataTable({
+    columns = names(colony_plot)
+    if (!is.null(input$select)) {
+      columns = input$select
+    }
+    colony_plot[,columns,drop=FALSE]
+    
+  })
+  
+  output$stressor_table = DT::renderDataTable({
+    columns_lost = names(stressor)
+    if (!is.null(input$select_lost)) {
+      columns_lost = input$select_lost
+    }
+    stressor[,columns_lost,drop=FALSE]
+    
+  })
+  
+  
 
     output$mapPlot <- renderPlot({
 
@@ -77,7 +100,7 @@ shinyServer(function(input, output) {
       )
       
       title = paste("Lost colonies in", input$year, "(%) *")
-      plot_usmap(data = df, labels=T) + labs(title = title) + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436"),limits = c(0, 50))
+      plot_usmap(data = df, labels=T) + labs(title = title) + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436"),limits = c(0, 50)) #+ geom_text()
 
         
 
@@ -353,6 +376,34 @@ shinyServer(function(input, output) {
       
     })
     
+    # BOXPLOT
+    
+    output$box_plots <- renderPlot({
+      n.str1.corr <- d %>% filter(year == input$year_box) %>%
+        group_by(
+          year,months) %>% 
+        select(year, months,colony_lost_pct) %>% 
+        summarise(`Average colony_lost_pct` = mean(colony_lost_pct))
+      
+      n.str1.corr_s <- n.str1.corr
+      
+      n <- dim(n.str1.corr)[1]
+      
+      for(i in 1:n){
+        ifelse((n.str1.corr$months[i] == "April-June" | n.str1.corr$months[i] == "July-September" ), 
+               n.str1.corr_s$months[i] <- "Summer",  n.str1.corr_s$months[i] <- "Winter")
+      }
+      title = paste('Loss percentage: Winter vs Summer in',input$year_box)
+      plot(as.factor(n.str1.corr_s$months), n.str1.corr$`Average colony_lost_pct`,xlab='period',ylab='loss percentage',
+           col=c("red","blue"),main=title)
+      
+      legend("bottomright", legend = c("Summer", "Winter"),
+             lwd = 3, col = c("red", "blue"))
+      
+    })
+    
+    # CLUSTERS
+    
     dist_mat <- reactive({
       dist(X, method=input$dist)
     })
@@ -398,5 +449,120 @@ shinyServer(function(input, output) {
       plot_usmap(data = df_cl) + labs(title = "Clusters") + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436"))
       
     })
+    
+    output$Map_s <- renderPlot({
+      
+      us_data <- map_data("state")
+      cv <- filter(stressor,year == input$year_s & stressor== input$stressor)
+      cv <- cv[,c(3,5)]
+      cv$state <- factor(cv$state)
+      cv[is.na(cv$stress_pct),2]<-0 
+      agg = aggregate(cv,
+                      by = list(cv$state),
+                      FUN = mean,
+      )
+      
+      agg <- agg[,-2]
 
+      df <- data.frame(
+        state = agg[,1],
+        values = agg[,2]
+      )
+      
+      title = paste("Stressor percentage in", input$year_s, "(%) *")
+      plot_usmap(data = df, labels=T) + labs(title = title) + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436"),limits = c(0, 50)) #+ geom_text()
+      
+      
+      
+    })
+    
+    output$mapPlot2 <- renderPlot({
+      
+      us_data <- map_data("state")
+      cv <- colony_plot[colony_plot$year == input$year_l,c(3,7)] #select state and colony lost percentage
+      cv$state <- factor(cv$state)
+      cv[is.na(cv$colony_lost_pct),2]<-0 
+      agg = aggregate(cv,
+                      by = list(cv$state),
+                      FUN = mean,
+      )
+      
+      agg <- agg[,-2]
+      
+      
+      
+      df <- data.frame(
+        state = agg[,1],
+        values = agg[,2]
+      )
+      
+      title = paste("Lost colonies in", input$year_l, "(%) *")
+      plot_usmap(data = df, labels=T) + labs(title = title) + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436")) #+ geom_text()
+      
+      
+      
+    })
+    
+    output$mapPlot2_s <- renderPlot({
+      
+      us_data <- map_data("state")
+      cv <- filter(stressor,year == input$year_l & stressor== input$stressor_l)
+      cv <- cv[,c(3,5)]
+      cv$state <- factor(cv$state)
+      cv[is.na(cv$stress_pct),2]<-0 
+       
+      agg = aggregate(cv,
+                      by = list(cv$state),
+                      FUN = mean,
+      )
+      
+      agg <- agg[,-2]
+      
+      
+      
+      
+      df <- data.frame(
+        state = agg[,1],
+        values = agg[,2]
+      )
+      
+      title = paste("Stressor pct", input$year_l, "(%) *")
+      plot_usmap(data = df, labels=T) + labs(title = title) + scale_fill_gradientn(colors = c("#1b98e0", "#f6f805", "#353436")) #+ geom_text()
+      
+      
+      
+    })
+    
+    output$plot_stressor_lost = renderPlot({
+      
+      state <- factor(colony$state)
+      i <- which(state=="California")
+      length(i)
+      x_time <- rep(1:30)
+      
+      
+      i_stres <- which(stressor$state == input$selected_state)
+      
+      i_s_var <- which(stressor[i_stres,"stressor"]=="Varroa mites")
+      i_s_op <- which(stressor[i_stres,"stressor"]=="Other pests/parasites")
+      i_s_dis <- which(stressor[i_stres,"stressor"]=="Disesases")
+      i_s_pes <- which(stressor[i_stres,"stressor"]=="Pesticides")
+      i_s_oth <- which(stressor[i_stres,"stressor"]=="Other")
+      i_s_unk <- which(stressor[i_stres,"stressor"]=="Unknown")
+      
+      
+      matplot(x_time, colony[i,"colony_lost_pct"], type="l", ylim=c(0,60), ylab="Percentage", main ="Trend of colony lost pct vs Stressors pct")
+      lines(x_time, stressor[i_s_var,"stress_pct"]$stress_pct, col = "red")
+      lines(x_time, stressor[i_s_op,"stress_pct"]$stress_pct, col = "green")
+      lines(x_time, stressor[i_s_dis,"stress_pct"]$stress_pct, col = "blue")
+      lines(x_time, stressor[i_s_pes,"stress_pct"]$stress_pct, col = "orange")
+      lines(x_time, stressor[i_s_oth,"stress_pct"]$stress_pct, col = "purple")
+      lines(x_time, stressor[i_s_unk,"stress_pct"]$stress_pct, col = "brown")
+      
+      legend("topright", legend=c("Colonylostpct","Varroa", "Other Pest", "Disesases", "Pesticides", "Other", "Unknown"),
+             col=c("black","red", "green", "blue", "orange", "purple", "brown" ), cex=0.8, lwd = 1)
+ 
+    
+})
+    
 })
--- Map_visualization/ui.R
+++ Map_visualization/ui.R
@@ -14,13 +14,23 @@ library(shinythemes)
 shinyUI(fluidPage(theme = shinytheme("flatly"),
 
     # Application title
-    titlePanel("Average Lost of bee colonies in US"),
+    titlePanel("Analysis of bee colonies Loss in US"),
                
 
     # Sidebar with a slider input for number of bins
     navbarPage("Sezioni",
         tabPanel(icon("home"),
-        p("This app has been developed to analyze data on bee colony loss (from USDA) "),         
+                 p("Data available (from USDA): colony loss and stressor associated"),         
+                 selectInput("select", "Select 1 or more columns to display", colnames(colony_plot), multiple = TRUE),
+                 fluidRow(column(DT::dataTableOutput("lost_table"),
+                                 width = 12)),  
+                 selectInput("select_lost", "Select 1 or more columns to display", colnames(stressor), multiple = TRUE),
+                 fluidRow(column(DT::dataTableOutput("stressor_table"),
+                                 width = 12)),    
+        ),
+        tabPanel("Colony loss",
+                 p("This app has been developed to analyze data on bee colony loss (from USDA) "),         
+                 
         sidebarPanel(
             sliderInput("year",
                         "Year:",
@@ -35,6 +45,7 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
             plotOutput("mapPlot")
         ),
         
+                
         p("* Each percentage has to be considered as average of quarter's proportions of lost colonies. Each quarter percentage is calculated as number of lost colonies over the maximum number of colonies in that period."),
         
         p("A total of 3,347 beekeepers from the United States that collectively managed 192,384 colonies on 1 October 2020 provided validated survey responses. 
@@ -44,7 +55,7 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
           Therefore, colony loss rates are best interpreted as a turn-over rate, as high levels of losses do not necessarily result in a decrease in the total number of colonies managed in the United States.")
         
         ),
-    tabPanel("Annual differences",
+    tabPanel("Annual differences 1",
              p("This tool allows you to visualize the (difference between) lost colonies in two years"),
              fluidRow(
                column(4,
@@ -88,7 +99,7 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
              
     ),
     
-    tabPanel("Seasonal trends",
+    tabPanel("2",
              p("This tool allows you to visualize how the percentage of lost colonies changes along the years for the same season "),
              fluidRow(
                column(4,
@@ -127,17 +138,33 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
                       plotOutput("MapP2"))
             ),
     ),
+    tabPanel("Seasonal trends",
+             fluidRow(
+               column(8,
+                      sliderInput("year_box",
+                                  "First year:",
+                                  min = 2015,
+                                  max = 2021,
+                                  value = 2016,
+                                  sep = "")
+                      )),
+             fluidRow(  
+               column(2),
+               column(8,
+             plotOutput("box_plots")),
+             )),
+             
     
-    tabPanel("Hierarchical Clusters",
+    tabPanel("Hier. Clusters",
              fluidRow(column(3,
                              selectInput('dist', 'Choose a distance', choices = list(
                                "Eucledian" = 'euclidean', "Manhattan" = 'manhattan', "Canberra" = 'canberra'),
-                               selected = 'euclidean')
+                               selected = 'canberra')
              ),
              column(3,
                     selectInput('linkage', 'Choose a Linkage', choices = list(
                       "Single" = 'single', "Average" = 'average', "Complete" = 'complete', "Ward"='ward.D2'),
-                      selected = 'average')
+                      selected = 'complete')
              ),
              
              column(3,
@@ -173,7 +200,69 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
              
              
              
+    ),
+    
+    tabPanel("Stressors",
+             p("This tool allows you to visualize the (difference between) lost colonies in two years"),
+    
+             
+             fluidRow(
+               column(4,
+                      selectInput("months_s", label = h4("Select period"), 
+                                  choices = c("Entire Year",unique(colony_plot$months)),#list("January-March" = "January-March", "April-June" = "April-June", "July-September" = "July-September"), 
+                                  selected = "Entire Year")),
+               column(4,
+                      
+                      
+                      sliderInput("year_s",
+                                  "First year:",
+                                  min = 2015,
+                                  max = 2022,
+                                  value = 2016,
+                                  sep = "")
+               ),
+               column(4,
+                      
+                      selectInput("stressor", label = h4("Select stressor"), 
+                                  choices = unique(stressor$stressor),#list("January-March" = "January-March", "April-June" = "April-June", "July-September" = "July-September"), 
+                                  selected = "Varroa mites")
+                      
+               )),
+             fluidRow(
+               plotOutput("Map_s")),
+             ),
+    
+    tabPanel("Loss vs Stressors",
+             fluidRow(
+               column(6,
+             sliderInput("year_l",
+                         "Year:",
+                         min = 2015,
+                         max = 2022,
+                         value = 2020,
+                         sep = ""),),
+             column(6,
+                    selectInput("stressor_l", label = h4("Select stressor"), 
+                                choices = unique(stressor$stressor),#list("January-March" = "January-March", "April-June" = "April-June", "July-September" = "July-September"), 
+                                selected = "Varroa mites"))
+             ),
+             
+             fluidRow(
+               column(6, plotOutput("mapPlot2")),
+               column(6, plotOutput("mapPlot2_s"))),
+             
+             selectInput("selected_state", label = h4("Select state"), 
+                         choices = unique(stressor$state),#list("January-March" = "January-March", "April-June" = "April-June", "July-September" = "July-September"), 
+                         selected = "California"),
+             
+             fluidRow(plotOutput("plot_stressor_lost")),
+               
+             
+             
+             
+             
     )
+    
             
 )
 ))
