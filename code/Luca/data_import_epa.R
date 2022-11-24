078d7d3d364d7b78b66191110d171bcf7d161c94
lucamainini <57671317+lucamainini@users.noreply.github.com>
2022-11-24 14:29:33 +0100

    Update data_import_epa.R

--- code/Luca/data_import_epa.R
+++ code/Luca/data_import_epa.R
@@ -1,2 +1,35 @@
 library(RAQSAPI)
-library("keyring") keyring::key_set(service = "AQSDatamart", username = "luca1.mainini@mail.polimi.it")
+library("keyring") 
+keyring::key_set(service = "AQSDatamart", username = "luca1.mainini@mail.polimi.it")
+
+datamartAPI_user <- "luca1.mainini@mail.polimi.it" 
+server <- "AQSDatamart"
+aqs_credentials(username = datamartAPI_user,
+                key = key_get(service = server,
+                              username = datamartAPI_user
+                )
+)
+
+# returns a tibble of all benzene annualy
+# North Carolina = 1995
+## parameter = 45201 #benzene
+data_extr <- aqs_annualsummary_by_state(parameter = "45201",
+bdate = as.Date("19950515",
+                format="%Y%m%d"
+),
+edate = as.Date("19950515",
+                format = "%Y%m%d"
+),
+stateFIPS = "37"
+)
+
+# direttamente per semestre
+temp <- aqs_quarterlysummary_by_state(parameter = "88101",
+                                      bdate = as.Date("20160101",
+                                                      format = "%Y%m%d"),
+                                      edate = as.Date("20170228",
+                                                      format = "%Y%m%d"),
+                                      stateFIPS = "37"
+)
+
+                
