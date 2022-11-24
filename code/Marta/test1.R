9c6f3844efd1af89558e6c2de947f0fb7db1207d
martacerri <114147758+martacerri@users.noreply.github.com>
2022-11-24 15:48:48 +0100

    test

---
+++ code/Marta/test1.R
@@ -0,0 +1,109 @@
+#understand if the mean loss of colony during summer is significantly different from the one in the winter
+#being Y(i_summer)∼iid Y(summer) and Y(i_winter)∼iid Y(winter), we want to test the equality of the two distributions
+
+##  H0:Y(summer)=Y(winter) vs H1:Y(summer)≠Y(winter)
+library(progress)
+data <- read.csv('cleaned_data.csv')
+data<-data[,-1]
+for(i in 1:n){
+  ifelse((data$months[i] == "April-June" | data$months[i] == "July-September" ), 
+         data$months[i] <- "Summer",  data$months[i] <- "Winter")
+}
+
+#per evitare la dipendenza faccio il test per ogni anno
+df_season <- read.csv('df_season.csv')
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2015")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2015")
+t1<-df_season[i2,4]  #summer population in 2015
+t2<-df_season[i3,4]  #winter population in 2015
+
+#p_val=1e-04
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2016")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2016")
+t1<-df_season[i2,4]  #summer population in 2016
+t2<-df_season[i3,4]
+
+#p_val=0.00661
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2017")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2017")
+t1<-df_season[i2,4]  #summer population in 2017
+t2<-df_season[i3,4]
+
+#p_val=1e-05
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2018")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2018")
+t1<-df_season[i2,4]  #summer population in 2018
+t2<-df_season[i3,4]
+
+#p_val=1e-05
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2019")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2019")
+t1<-df_season[i2,4]  #summer population in 2019
+t2<-df_season[i3,4]
+
+#p_val=0.10453
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2020")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2020")
+t1<-df_season[i2,4]  #summer population in 2020
+t2<-df_season[i3,4]
+
+#p_val=0.52872
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2021")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2021")
+t1<-df_season[i2,4]  #summer population in 2021
+t2<-df_season[i3,4]
+
+#p_val=1e-04
+
+i2<-which(df_season$season=="Summer"&df_season$year_revisited=="2022")
+i3<-which(df_season$season=="Winter"&df_season$year_revisited=="2022")
+t1<-df_season[i2,4]  #summer population in 2022
+t2<-df_season[i3,4]
+
+#p_val=0.00229
+
+#paired population
+delta.0 <- 0
+diff <- t1-t2
+diff.mean <- mean(diff)
+diff.cov <- var(diff)
+
+#euclidean distance between the difference in means and the hypothesised value
+T20 <- abs(diff.mean-delta.0) 
+#Mahalanobis distance, but "forgetting" about the covariance between the values
+#T20 <- as.numeric( (diff.mean-delta.0) %*% solve(diag(diag(diff.cov))) %*% (diff.mean-delta.0))
+
+B = 100000
+seed = 26111992
+n1 <- length(t1)[1]
+n2 <- length(t2)[1]
+n <- n1+n2
+T2 <- numeric(B)
+set.seed(seed)
+for(perm in 1:B)
+{
+  # Random permutation
+  # obs: exchanging data within couples means changing the sign of the difference
+  signs.perm <- rbinom(n1, 1, 0.5)*2 - 1
+  diff_perm <- diff * matrix(signs.perm,nrow=n1,ncol=1,byrow=FALSE)
+  diff.mean_perm <- mean(diff_perm)
+  T2[perm] <- abs(diff.mean_perm-delta.0)
+  }
+# plotting the permutational distribution under H0
+hist(T2,xlim=range(c(T2,T20)),breaks=100)
+abline(v=T20,col=3,lwd=4)
+
+plot(ecdf(T2))
+abline(v=T20,col=3,lwd=4)
+
+# p-value
+p_val <- sum(T2>=T20)/B
+p_val
+
