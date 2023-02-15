24c29a3404b2c13fd53a36059972d81336db8fb9
lucamainini <57671317+lucamainini@users.noreply.github.com>
2023-02-15 12:06:39 +0100

    add test object

--- code/Lupo/TimeSpatial_Inference.R
+++ code/Lupo/TimeSpatial_Inference.R
@@ -1,11 +1,31 @@
+
+rm(list=ls())
+graphics.off()
+set.seed(1)
+
+library(ISLR2)
+library(car)
+library(mgcv)
+library(rgl)
+library(splines)
+library(pbapply)
+library(np)
+library(fda)
+library(dplyr)
+
+library(RColorBrewer)
+library(sf)
+library(fdaPDE)
+library(viridis)
+library(latex2exp)
+
+
 ###############################################################################.
 ####     SPACE TIME MODEL WITH A MANUAL MESH, TARGET = COLONY LOSS PCT     ####
+####               INFERENCE ON COVARIATES PARAMETERS                      ####
 ###############################################################################.
 
-#NO COVARIATES!
-
-#load already computed model
-#load("/Users/lupomarsigli/Desktop/NP_project/NP_github/np_project/code/Lupo/smoothing_iso_temp_06022022.Rdata")
+####### MODEL WITH COVARIATES #######.
 
 df <- read.csv("data/new_data/data_bystate_temp_perc.csv")
 
@@ -29,21 +49,7 @@ col_order <- order(colnames(data))
 col_order <- c(1,head(col_order,-1))
 data <- data[,col_order]
 
-boundary_all <- read.table("code/Lupo/boundary.txt", header=TRUE)
-
-#boundary <- data.frame(
-#  long = c(min(boundary_all$long), max(boundary_all$long), max(boundary_all$long), min(boundary_all$long)),
-#  lat = c(max(boundary_all$lat), max(boundary_all$lat), min(boundary_all$lat), min(boundary_all$lat)))
-
-#from top left --> top right --> bottom right --> bottom left
-#long <- c(min(boundary_all$long), -95, -85, -80, max(boundary_all$long), max(boundary_all$long), -77,
-#          -80, -83, -84, -95, -97, min(boundary_all$long))
-#lat <- c(max(boundary_all$lat), max(boundary_all$lat), 47, 43, max(boundary_all$lat), 40, 32,
-#         min(boundary_all$lat), min(boundary_all$lat), 28, 28, 25, 35)
-#boundary <-  data.frame( long=long, lat=lat)
-
-#Boundary Gege:
-boundary <- read.table("/Users/lupomarsigli/Desktop/NP_project/NP_github/np_project/code/Eugenio/boundary_gg.txt", head=T)
+boundary <- read.table("code/Eugenio/boundary_gg.txt", head=T)
 
 df_coord <- read.csv("data/state_coords_lon_lat.csv")
 #remove hawaii:
@@ -80,7 +86,7 @@ mesh <- refine.mesh.2D(mesh_1, maximum_area=1.5, minimum_angle = 30)
 basisobj <- create.FEM.basis(mesh)
 
 #plot the mesh
-shapefile <- "/Users/lupomarsigli/Desktop/NP_project/NP_github/np_project/code/Lupo/cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
+shapefile <- "code/Lupo/cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
 orotl_sf <- st_read(shapefile)
 par(mar=c(0,0,0,0))
 plot(mesh,asp=1, pch=".")
@@ -89,54 +95,6 @@ points(mesh$nodes[which(mesh$nodesmarkers==0),], pch=16,cex=0.6)
 points(mesh$nodes[which(mesh$nodesmarkers==1),], pch=16, col='red',cex=0.6)
 points(p,pch=16,cex=1.5,col=c(rep('blue',dim(data_locations)[1]),rep('orange',dim(boundary)[1])))
 plot(st_geometry(orotl_sf), width = 3, add=T) #col="blue"
-
-lambdaS=10^-1
-lambdaT=10^-1
-
-start_time <- Sys.time()
-smoothing_temp_iso <- smooth.FEM.time(locations=data_locations, time_locations=time_locations,
-                                      observations=data_loss, 
-                                      FEMbasis=basisobj,
-                                      #lambdaS=lambdaS,
-                                      #lambdaT=lambdaT
-                                      lambda.selection.criterion='newton_fd', 
-                                      lambda.selection.lossfunction='GCV',
-                                      DOF.evaluation='stochastic')
-end_time <- Sys.time()
-final_time = end_time - start_time
-
-lambdaS <- smoothing_temp_iso$optimization$lambda_solution[1]
-lambdaT <- smoothing_temp_iso$optimization$lambda_solution[2]
-#lambdaS and lambdaT equals to the last row of matrix: smoothing_temp_iso$optimization$lambda_vector
-
-#save(smoothing_temp_iso, file ="code/Lupo/smoothing_iso_temp_06022022.Rdata")
-
-#does not work!
-open3d()
-mfrow3d(3, 1, byrow = TRUE, sharedMouse = FALSE)
-plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations) #axes=FALSE
-#axes3d(col='white')
-
-#does not work!
-open3d()
-mfrow3d(3, 2, byrow = T)
-for (i in 1:6) {
-  plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations) 
-}
-highlevel(integer()) # To trigger display as rglwidget
-
-plot(smoothing_temp_iso$fit.FEM.time, locations=data_locations[4,])
-
-#play3d(spin3d())
-#play3d(spin3d(axis = c(1,0,0), rpm = 4), duration = 10)
-
-coeffs <- smoothing_temp_iso$fit.FEM.time$coeff
-range(coeffs)
-
-time_instant <- 5 #from 1 to 29
-plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations[time_instant]) #lambdaS = 1, lambdaT = 1
-
-plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations)
 
 ###############################################################################.
 ##########                  ADD COVARIATES                          ##########
@@ -151,70 +109,42 @@ df <- df[df$state != "hawaii" & df$state != "other states",]
 df_covariates <- df[,c(1,2,3,11,12,13,14,15,16,17,18,20,21)]
 
 #First alternative to build covariates matrix
-ncov <- 6
+ncov <- 4
 covariates <- matrix(NA,nrow=dim(df_covariates)[1],ncol=ncov)
 covariates[,1] <- as.numeric(df_covariates[,4]) #varroa
 covariates[,2] <- as.numeric(df_covariates[,7]) #pesticides
 covariates[,3] <- as.numeric(df_covariates[,8]) #other
 covariates[,4] <- as.numeric(df_covariates[,9]) #unknown
-covariates[,5] <- as.numeric(df_covariates[,11]) #min temp
-covariates[,6] <- as.numeric(df_covariates[,12]) #precipitation
+#covariates[,5] <- as.numeric(df_covariates[,11]) #min temp
+#covariates[,6] <- as.numeric(df_covariates[,12]) #precipitation
 
 # CHECK THE COVARIATE MATRIX IS IN THE RIGHT ORDER OF TIME AND SPACE: here
 #the order is with time t associated to all the states, followed by time t+1
 #associated to all the states...
 
-#smoothing_temp_cov_iso and smoothing_temp_cov_iso2 are the models with covariates
-#rows ordered as described above
-load("/Users/lupomarsigli/Desktop/NP_project/NP_github/np_project/code/Lupo/smoothing_temp_cov_iso_VarroaDisPest_08022022.Rdata")
-
-############################################################################.
-
-# Let's try with the covariate matrices inverted, meaning that, on the rows,
-# the data is listed with a state and all the possible times, followed
-# by the next state in alphabetical order with all the times, ...
-
-df_covariates <- df_covariates %>% arrange(state, year, months)
-ncov <- 3
-covariates <- matrix(NA,nrow=dim(df_covariates)[1],ncol=ncov)
-covariates[,1] <- as.numeric(df_covariates[,4]) #varroa
-covariates[,2] <- as.numeric(df_covariates[,7]) #pesticides
-covariates[,3] <- as.numeric(df_covariates[,8]) #other
-#covariates[,4] <- as.numeric(df_covariates[,9]) #unknown
-#covariates[,5] <- as.numeric(df_covariates[,11]) #min temp
-#covariates[,6] <- as.numeric(df_covariates[,12]) #precipitation
-
-#smoothing_temp_cov_iso3 is the model with covariates and rows ordered as described above
-#load("/Users/lupomarsigli/Desktop/NP_project/NP_github/np_project/code/Lupo/smoothing_temp_covinv_iso_VarroaPestOther_09022022.Rdata")
-
-############################################################################.
-
 #QUESTION: does the matrix of covariates, in the rows, should have first the same state for each time
 #or first each time followed by all the state ?????
 
+obj_inf <- inferenceDataObjectBuilder(test = 'sim', type = 'esf', component = "parametric",
+                                      dim = 2, n_cov = ncov, beta0 = rep(0,ncov))
+
 start_time <- Sys.time()
-smoothing_temp_cov_iso <- smooth.FEM.time(locations=data_locations, time_locations=time_locations,
+smooth_temp_inference <- smooth.FEM.time(locations=data_locations, time_locations=time_locations,
                                           observations=data_loss, 
                                           FEMbasis=basisobj,
                                           covariates = covariates,
                                           lambda.selection.criterion='newton_fd', 
                                           lambda.selection.lossfunction='GCV',
-                                          DOF.evaluation='stochastic')
+                                          DOF.evaluation='stochastic',
+                                          inference.data.object = obj_inf)
 end_time <- Sys.time()
 final_time = end_time - start_time
 
-
-#save(smoothing_temp_cov_iso , file ="code/Lupo/smoothing_temp_cov_iso_VarroaDisPest_08022022.Rdata")
-#save(smoothing_temp_cov_iso2 , file ="code/Lupo/smoothing_temp_cov_iso_VarroaPestOtherUnknMinTempPrec_09022022.Rdata")
-#save(smoothing_temp_cov_iso3 , file ="code/Lupo/smoothing_temp_covinv_iso_VarroaPestOther_09022022.Rdata")
-
-smoothing_temp_cov_iso$solution$rmse
-f <- smoothing_temp_cov_iso$solution$f
-g <- smoothing_temp_cov_iso$solution$g
-
-smoothing_temp_cov_iso$beta
-smoothing_temp_cov_iso$optimization$lambda_solution
-smoothing_temp_cov_iso$optimization$GCV_vector
+smooth_temp_inference$solution$rmse
+smooth_temp_inference$beta
+smooth_temp_inference$optimization$lambda_solution
+smooth_temp_inference$optimization$GCV_vector
+smooth_temp_inference$inference$beta
 
 #3d plot
 time_instant <- 5 #from 1 to 29
@@ -223,14 +153,12 @@ points3d(data_locations[,1], data_locations[,2], data_loss[,time_instant], cex=1
 
 plot(smoothing_temp_cov_iso$fit.FEM.time, time_locations=time_locations[time_instant])
 
-states <- df_new$state
-text3D(data_locations[,1], data_locations[,2], data_loss[,time_instant], states,
-       colvar = data_loss[,time_instant], col = gg.col(100), theta = 60, phi = 20,
-       xlab = "Longitude", ylab = "Latitude", zlab = "Colony Loss pct", 
-       main = "Colony Loss Percentage in USA at t=time_instant", cex = 0.6,
-       ticktype = "detailed", font = 2) #bty = "g", d = 2, adj = 0.5
-
 rgl.postscript("....pdf", fmt="pdf")
+
+time_instant <- 5 #from 1 to 29
+plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations[time_instant]) #lambdaS = 1, lambdaT = 1
+
+plot(smoothing_temp_iso$fit.FEM.time, time_locations=time_locations)
 
 #2d plot
 image(smoothing_temp_cov_iso$fit.FEM.time, t=time_locations[c(1,10,29)])
@@ -293,11 +221,27 @@ for (t in time_instants){
   plot(st_geometry(orotl_sf), width=2, add=T)
 }
 
-#outcome: adding the covariates in the case of colony_loss_pct does not change that
-#much in terms of rmse, and the estimate values of betas parameter are all really
-#low, around 0.001 / 0.005. Moreover, GCV around 46-48.
-#case with covariates are the models: smoothing_temp_cov_iso, smoothing_temp_cov_iso2,
-#smoothing_temp_cov_iso3
 
-#The case without covariates, i.e. smoothing_temp_iso has GCV around 48 and rmse
-#around 6.38
+########
+
+lambdaS=10^-1
+lambdaT=10^-1
+
+start_time <- Sys.time()
+smoothing_temp_iso <- smooth.FEM.time(locations=data_locations, time_locations=time_locations,
+                                      observations=data_loss, 
+                                      FEMbasis=basisobj,
+                                      #lambdaS=lambdaS,
+                                      #lambdaT=lambdaT
+                                      lambda.selection.criterion='newton_fd', 
+                                      lambda.selection.lossfunction='GCV',
+                                      DOF.evaluation='stochastic')
+end_time <- Sys.time()
+final_time = end_time - start_time
+
+lambdaS <- smoothing_temp_iso$optimization$lambda_solution[1]
+lambdaT <- smoothing_temp_iso$optimization$lambda_solution[2]
+#lambdaS and lambdaT equals to the last row of matrix: smoothing_temp_iso$optimization$lambda_vector
+
+#save(smoothing_temp_iso, file ="code/Lupo/smoothing_iso_temp_06022022.Rdata")
+
